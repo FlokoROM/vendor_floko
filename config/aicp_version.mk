4c2e315cdb6db809b8c813f6d465c67004354863
lindwurm <lindwurm.q@gmail.com>
2019-01-11 22:02:52 +0900

    floko: Start our versioning
    Signed-off-by: lindwurm <lindwurm.q@gmail.com>

--- config/aicp_version.mk
+++ config/aicp_version.mk
@@ -3,8 +3,8 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_VERSION_TAGS=release-keys BUILD_UTC_DATE=$
 
 AICP_BRANCH=p
 
-# AICP RELEASE VERSION
-AICP_VERSION_MAJOR = 14
+# RELEASE VERSION
+AICP_VERSION_MAJOR = 9
 AICP_VERSION_MINOR = 0
 AICP_VERSION_MAINTENANCE = 0
 
@@ -21,20 +21,20 @@ endif
 
 ifdef AICP_BUILDTYPE
     ifeq ($(AICP_BUILDTYPE), NIGHTLY)
-        AICP_VERSION := $(TARGET_PRODUCT)_$(AICP_BRANCH)-$(VERSION)-NIGHTLY-$(shell date -u +%Y%m%d)
+        AICP_VERSION := floko_$(AICP_BUILD)_$(AICP_BRANCH)-$(VERSION)-NIGHTLY-$(shell date -u +%Y%m%d)
     endif
     ifeq ($(AICP_BUILDTYPE), WEEKLY)
-       AICP_VERSION := $(TARGET_PRODUCT)_$(AICP_BRANCH)-$(VERSION)-WEEKLY-$(shell date -u +%Y%m%d)
+       AICP_VERSION := floko_$(AICP_BUILD)_$(AICP_BRANCH)-$(VERSION)-WEEKLY-$(shell date -u +%Y%m%d)
     endif
     ifeq ($(AICP_BUILDTYPE), EXPERIMENTAL)
-        AICP_VERSION := $(TARGET_PRODUCT)_$(AICP_BRANCH)-$(VERSION)-EXPERIMENTAL-$(shell date -u +%Y%m%d)
+        AICP_VERSION := floko_$(AICP_BUILD)_$(AICP_BRANCH)-$(VERSION)-EXPERIMENTAL-$(shell date -u +%Y%m%d)
     endif
     ifeq ($(AICP_BUILDTYPE), UNOFFICIAL)
-        AICP_VERSION := $(TARGET_PRODUCT)_$(AICP_BRANCH)-$(VERSION)-UNOFFICIAL-$(shell date -u +%Y%m%d)
+        AICP_VERSION := floko_$(AICP_BUILD)_$(AICP_BRANCH)-$(VERSION)-UNOFFICIAL-$(shell date -u +%Y%m%d)
     endif
 else
 #We reset back to UNOFFICIAL
-        AICP_VERSION := $(TARGET_PRODUCT)_$(AICP_BRANCH)-$(VERSION)-UNOFFICIAL-$(shell date -u +%Y%m%d)
+        AICP_VERSION := floko_$(AICP_BUILD)_$(AICP_BRANCH)-$(VERSION)-UNOFFICIAL-$(shell date -u +%Y%m%d)
 endif
 
 # AICP System Version
@@ -45,14 +45,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.aicp.version.update=$(AICP_BRANCH)-$(VERSION) \
     ro.aicp.build.version=$(VERSION) \
     ro.aicp.display.version=$(AICP_VERSION)
-
-# needed for statistics
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.aicp.branch=$(AICP_BRANCH) \
-    ro.romstats.url=https://stats.aicp-rom.com/ \
-    ro.romstats.name=AICP \
-    ro.romstats.buildtype=$(AICP_BUILDTYPE) \
-    ro.romstats.version=$(VERSION) \
-    ro.romstats.tframe=1 \
-    ro.romstats.askfirst=1 \
-    ro.romstats.ga=UA-48128535-2
