ec867681b703ad5f0c15d8834d3ee7336f53cd75
lindwurm <lindwurm.q@gmail.com>
2019-01-11 22:00:04 +0900

    vendor: no USER=android-build
    Signed-off-by: lindwurm <lindwurm.q@gmail.com>

--- config/aicp_version.mk
+++ config/aicp_version.mk
@@ -1,5 +1,5 @@
 # Version information used on all builds
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_VERSION_TAGS=release-keys BUILD_UTC_DATE=$(shell date +"%s")
 
 AICP_BRANCH=p
 
