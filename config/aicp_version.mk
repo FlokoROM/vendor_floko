2d94fb5026cd3de67c4dcabb0679f6ef8caf86eb
Michele Bono <bono.michele94@gmail.com>
2019-02-05 16:46:45 +0100

    vendor: fix our versioning
    Change-Id: Ic26c11a25eaad0387e2642674743fffce81978e3
    Signed-off-by: lindwurm <lindwurm.q@gmail.com>

--- config/aicp_version.mk
+++ config/aicp_version.mk
@@ -8,6 +8,10 @@ AICP_VERSION_MAJOR = 9
 AICP_VERSION_MINOR = 0
 AICP_VERSION_MAINTENANCE = 0
 
+PRODUCT_VERSION_MAJOR := $(AICP_VERSION_MAJOR)
+PRODUCT_VERSION_MINOR := $(AICP_VERSION_MINOR)
+PRODUCT_VERSION_MAINTENANCE := $(AICP_VERSION_MAINTENANCE)
+
 VERSION := $(AICP_VERSION_MAJOR).$(AICP_VERSION_MINOR)
 
 ifndef AICP_BUILDTYPE
@@ -39,9 +43,13 @@ endif
 
 # AICP System Version
 PRODUCT_PROPERTY_OVERRIDES += \
+    BUILD_DISPLAY_ID=$(BUILD_ID) \
+    aicp.ota.version=$(AICP_VERSION_MAJOR).$(AICP_VERSION_MINOR).$(AICP_VERSION_MAINTENANCE) \
     ro.modversion=$(VERSION)-$(AICP_BUILDTYPE) \
     ro.aicp.releasetype=$(AICP_BUILDTYPE) \
     ro.aicp.version=$(VERSION)-$(AICP_BUILDTYPE) \
     ro.aicp.version.update=$(AICP_BRANCH)-$(VERSION) \
     ro.aicp.build.version=$(VERSION) \
-    ro.aicp.display.version=$(AICP_VERSION)
+    ro.aicp.display.version=$(AICP_VERSION) \
+    ro.aicp.buildtype=$(AICP_BUILDTYPE)
+
