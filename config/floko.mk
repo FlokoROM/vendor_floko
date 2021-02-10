987c1a7429489fd84980192bebc36883ab10d7ea
Pranav Vashi <neobuddy89@gmail.com>
2021-02-10 09:40:56 +0530

    crdroid: Build QS tile styles
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/floko.mk
+++ config/floko.mk
@@ -147,3 +147,32 @@ PRODUCT_PACKAGES += \
     GesturalNavigationOverlayLong \
     GesturalNavigationOverlayMedium \
     GesturalNavigationOverlayHidden
+
+# QS tile styles
+PRODUCT_PACKAGES += \
+    QStileDefault \
+    QStileCircleTrim \
+    QStileCircleDualTone \
+    QStileCircleGradient \
+    QStileCookie \
+    QStileCosmos \
+    QStileDividedCircle \
+    QStileDottedCircle \
+    QStileDualToneCircle \
+    QStileInk \
+    QStileInkdrop \
+    QStileJustIcons \
+    QStileMountain \
+    QStileNeonLike \
+    QStileNinja \
+    QStileOreo \
+    QStileOreoCircleTrim \
+    QStileOreoSquircleTrim \
+    QSTileOxygen \
+    QStilePokesign \
+    QStileSquaremedo \
+    QStileSquircle \
+    QStileSquircleTrim \
+    QStileTeardrop \
+    QStileTriangle \
+    QStileWavey
