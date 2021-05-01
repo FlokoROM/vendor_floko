90d604671d41ba05988aa2d1750a3a5e18b26e05
Julian Veit <Claymore1298@gmail.com>
2021-05-01 11:56:15 +0200

    bacon: switch to sha256 checksum
    As md5 is not considered as secure since some time, and sha256 is
    industry standard, lets switch over here too.
    
    Change-Id: I22015be20126a4348affa5ef97e2c28fbd7b4717
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: lindwurm <lindwurm.q@gmail.com>

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -18,11 +18,11 @@
 
 LINEAGE_TARGET_PACKAGE := $(PRODUCT_OUT)/$(LINEAGE_VERSION).zip
 
-MD5 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/md5sum
+SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
 
 .PHONY: bacon
 bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) mv -f $(INTERNAL_OTA_PACKAGE_TARGET) $(LINEAGE_TARGET_PACKAGE)
-	$(hide) $(MD5) $(LINEAGE_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(LINEAGE_TARGET_PACKAGE).md5sum
+	$(hide) $(SHA256) $(LINEAGE_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(LINEAGE_TARGET_PACKAGE).sha256sum
 	$(hide) rm -rf $(call intermediates-dir-for,PACKAGING,target_files)
 	@echo "Package Complete: $(LINEAGE_TARGET_PACKAGE)" >&2
