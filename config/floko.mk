bb3bbe03d9229f9354d0632ae2b48a6c14381424
Pranav Vashi <neobuddy89@gmail.com>
2021-07-24 11:06:00 +0530

    PebbleBerry: Rounded Corner styles [3/4]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/floko.mk
+++ config/floko.mk
@@ -204,6 +204,15 @@ PRODUCT_PACKAGES += \
     StockSwitch \
     TelegramSwitch
 
+# Rounded Styles
+PRODUCT_PACKAGES += \
+    StockRounded \
+    NoneRounded \
+    SlightRounded \
+    MediumRounded \
+    HighRounded \
+    ExtremeRounded
+
 # Volume Panel styles
 PRODUCT_PACKAGES += \
 	AospPanel \
